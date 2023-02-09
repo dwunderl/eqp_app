fb4e09b59be339b2fc98d8271290c9a56d0a9fbb
dwunderl <Ted@0995>
2023-02-08 17:23:31 -0700

    Improved naming of items between server and html

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index6.html')
+    return render_template('index.html')
 
 
 # called from flask ui
@@ -13,11 +13,11 @@ def index():
 
 @app.route("/submit", methods=["POST"])
 def process():
-    n1 = request.form["input1"]
-    n2 = request.form["input2"]
-    n3 = request.form["input3"]
-    n4 = request.form["input4"]
-    goal = request.form["input5"]
+    n1 = request.form["n1"]
+    n2 = request.form["n2"]
+    n3 = request.form["n3"]
+    n4 = request.form["n4"]
+    goal = request.form["goal"]
     print("before serveEqPuzzle: n1=",n1, ", goal=",goal)
 
     result = serveEqPuzzle(n1, n2, n3, n4, goal)
@@ -28,7 +28,7 @@ def process():
         print(sol.expression)
         retExps.append(sol.expression)
 
-    return render_template("index6.html", input1=n1, input2=n2, input3=n3, input4=n4, input5=goal, output_area=retExps)
+    return render_template("index.html", n1=n1, n2=n2, n3=n3, n4=n4, goal=goal, expressions=retExps)
 
 #---------------------------------
 if __name__ == "__main__":
